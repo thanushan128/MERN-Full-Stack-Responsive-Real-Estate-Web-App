91a03cb655fce8a0f0e1a249f7de2b248d25c09f
thanushan128 <thanushanv128@gmail.com>
2023-09-07 21:20:38 +0530

    Complete residency end point

---
+++ server/routes/residencyRoute.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { createResidency } from '../controllers/residencyController.js';
+
+const router = express.Router()
+
+router.post("/create", createResidency)
+
+export { router as residencyRoute }
